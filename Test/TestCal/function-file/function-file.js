81e72ea4c732147cf114e49807f5a81bb6faa92c
Bob German <bgerman1@hotmail.com>
2018-09-12 18:38:36 -0400

    Works but still very dodgy

--- Test/TestCal/function-file/function-file.js
+++ Test/TestCal/function-file/function-file.js
@@ -19,47 +19,56 @@ var settingsDialog;
 
 function insertDefaultGist(event) {
 
-  // Check if the add-in has been configured
-  if (config && config.defaultGistId) {
-    // Get the default Gist content and insert
-    try {
-      getGist(config.defaultGistId, function(gist, error) {
-        if (gist) {
-          buildBodyContent(gist, function (content, error) {
-            if (content) {
-              Office.context.mailbox.item.body.setSelectedDataAsync(content,
-                {coercionType: Office.CoercionType.Html}, function(result) {
-                  event.completed();
-              });
-            } else {
-              showError(error);
-              event.completed();
-            }
-          });
+  var gapMinutes = 10;               // Time gap in minutes
+  var gapMsec = gapMinutes * 60000;  // Time gap in milliseconds
+
+  // var getOptions = {
+  //   // Pass information that can be used
+  //   // in the callback
+  //     asyncContext: {verb:"Get"}
+  // }
+  // var setOptions = {
+  //   // Pass information that can be used
+  //   // in the callback
+  //     asyncContext: {verb:"Set"}
+  // }
+
+  // Get the existing end time
+  Office.context.mailbox.item.end.getAsync({
+    asyncContent: {verb: "Get"}
+  }, function(result) {
+    if (result.error) {
+      console.debug(result.error);
+    } else {
+
+      // Adjust the end time
+      var endTime = result.value;
+      message = "No time adjustment made";
+      if (!(endTime.getMinutes() % 15)) {
+        endTime = new Date (endTime.getTime() - gapMsec);
+        message = "Added a " + gapMinutes + " minute gap";
+      } else if (!((endTime.getMinutes()+gapMinutes) % 15)) {
+        endTime = new Date (endTime.getTime() + gapMsec);
+        message = "Removed " + gapMinutes + " minute gap";
+      }
+
+      Office.context.mailbox.item.end.setAsync(endTime, {
+        asyncContext: {verb:"Set"}
+      }, function(result) {
+        if (result.error) {
+          console.debug(result.error);
         } else {
-          showError(error);
-          event.completed();
+          Office.context.mailbox.item.body.setSelectedDataAsync(
+            message + "!<br />",
+            {coercionType: Office.CoercionType.Html}, function(result) {
+              event.completed();
+          });
+          console.debug("TestCal succeeded: " + message);
         }
       });
-    } catch (err) {
-      showError(err);
-      event.completed();
     }
+  });
 
-  } else {
-    // Save the event object so we can finish up later
-    btnEvent = event;
-    // Not configured yet, display settings dialog with
-    // warn=1 to display warning.
-    var url = new URI('../settings/dialog.html?warn=1').absoluteTo(window.location).toString();
-    var dialogOptions = { width: 20, height: 40, displayInIframe: true };
-
-    Office.context.ui.displayDialogAsync(url, dialogOptions, function(result) {
-      settingsDialog = result.value;
-      settingsDialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, receiveMessage);
-      settingsDialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogEventReceived, dialogClosed);
-    });
-  }
 }
 
 function receiveMessage(message) {
